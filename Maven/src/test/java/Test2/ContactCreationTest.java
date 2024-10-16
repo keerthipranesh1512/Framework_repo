709b721dbacefca54c27c44fbcc07d2dd096173d
pranesh <pranesh@DESKTOP-GSEU8SP>
2024-10-16 10:55:09 +0530

    My parameter

--- Maven/src/test/java/Test2/ContactCreationTest.java
+++ Maven/src/test/java/Test2/ContactCreationTest.java
@@ -6,6 +6,14 @@ public class ContactCreationTest {
 @Test
 public void creContactTest()
 {
+	String url=System.getProperty("url");
+	String browser=System.getProperty("browser");
+	String username=System.getProperty("username");
+	String pass=System.getProperty("pass");
+	System.out.println(url);
+	System.out.println(browser);
+	System.out.println(username);
+	System.out.println(pass);
 	System.out.println("execute create contact test");
 }
 @Test
